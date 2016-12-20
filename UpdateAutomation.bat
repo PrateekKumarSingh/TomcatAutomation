9a0388b42f42eaa96fdb6556129bdfcd426b57e2
Prateek Singh <Prateeksingh1590@gmail.com>
2016-12-21 00:44:27 +0530

    Update UpdateAutomation.bat

--- UpdateAutomation.bat
+++ UpdateAutomation.bat
@@ -1,12 +1,13 @@
 @echo off
 
-SET FileToCheck="P:\Freelancing\Root.war"
-SET TomcatBinDir="C:\Program Files\Apache Software Foundation\Tomcat 8.5\bin\"
+SET "FileToCheck=c:\data\Freelancing\Root.war"
+SET "TomcatBinDir=C:\Program Files\Apache Software Foundation\Tomcat 8.5\bin\"
 
-If exist %FileToCheck% (
+If exist "%FileToCheck%" (
 Echo %FileToCheck% exists, Stopping Tomcat now.
-%TomcatBinDir%Shutdown.bat
-
-) else (File not there.Sit Idle)
+"%TomcatBinDir%Shutdown.bat"
+copy "c:\data\Freelancing\ROOT.war" "C:\Program Files\Apache Software Foundation\Tomcat 8.5\bin"
+PAUSE>nul
+) else (echo File not there. Sit Idle)
 
 PAUSE>nul
