f101e7f92f923c1c7b7544405a6fa879a8a432ca
Prateek Singh <Prateeksingh1590@gmail.com>
2017-03-15 14:07:19 +0530

    Update UpdateAutomation.bat

--- UpdateAutomation.bat
+++ UpdateAutomation.bat
@@ -1,13 +1,26 @@
 @echo off
 
-SET "FileToCheck=c:\data\Freelancing\Root.war"
-SET "TomcatBinDir=C:\Program Files\Apache Software Foundation\Tomcat 8.5\bin\"
+SET "TomcatDir=C:\Program Files (x86)\Apache Software Foundation\Tomcat 8.5\"
+SET "FileToCheck=%TomcatDir%Update\Root.war"
+SET "TomcatBinDir=%TomcatDir%bin\"
+SET "TomcatWebAppDir=%TomcatDir%Webapps"
+SET "SecondsToSleep=60"
 
 If exist "%FileToCheck%" (
-Echo %FileToCheck% exists, Stopping Tomcat now.
-"%TomcatBinDir%Shutdown.bat"
-copy "c:\data\Freelancing\ROOT.war" "C:\Program Files\Apache Software Foundation\Tomcat 8.5\bin"
-PAUSE>nul
-) else (echo File not there. Sit Idle)
+Echo "INFORMATION : Found an Update at %FileToCheck% , Stopping Tomcat server now."
+cd "%TomcatBinDir%"
+"%TomcatBinDir%Shutdown.bat">nul
+timeout /t 20 /nobreak
+Echo "INFORMATION : Backing-up current Root.war file."
+mkdir "%TomcatDir%backup"
+copy "%TomcatWebAppDir%\Root.war" "%TomcatDir%backup" /Y
 
-PAUSE>nul
+Echo "INFORMATION : Removing old files %TomcatWebAppDir%\root"
+rmdir "%TomcatWebAppDir%\root" /s /q
+Echo "INFORMATION : Copying the update FROM '%FileToCheck%' TO '%TomcatWebAppDir%'."
+copy "%FileToCheck%" "%TomcatWebAppDir%" /Y
+Echo INFORMATION : Restarting Tomcat server now.
+"%TomcatBinDir%Startup.bat">nul
+Echo "INFORMATION : Sleeping for %SecondsToSleep% seconds, to allow Tomcat server to start."
+timeout /t %SecondsToSleep% /nobreak
+) else (echo Update file unpresent. Sit Idle)
